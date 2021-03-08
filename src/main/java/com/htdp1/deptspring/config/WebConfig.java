0cb1e84f17515b163f6fe950f81b76141fbc835d
jaemyeong.lee <robin022@naver.com>
2021-03-08 13:26:57 +0900

    .

---
+++ src/main/java/com/htdp1/deptspring/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.htdp1.deptspring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.htdp1.deptspring.interceptor.LoggerInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+	public @Value("${session.enabled}") boolean enabled;
+
+	/*
+	 * 로그인 인증 Interceptor 설정
+	 */
+	@Autowired
+	LoggerInterceptor loggerInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		log.debug("enabled => " + enabled);
+		
+		if (enabled) {
+			log.debug("enabled => " + enabled);
+			registry.addInterceptor(loggerInterceptor)
+				//.addPathPatterns("/v1/*")
+			;
+		}
+	}
+
+}
---
+++ src/main/java/com/htdp1/deptspring/interceptor/LoggerInterceptor.java
@@ -0,0 +1,42 @@
+package com.htdp1.deptspring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+
+		log.debug("session.getId() => " + session.getId());
+
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
