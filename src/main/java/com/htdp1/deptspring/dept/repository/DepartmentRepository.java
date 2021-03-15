26da51317e45b629d9fb2b4daff2df65bc7eb7d6
jaemyeong.lee <robin022@naver.com>
2021-03-15 11:28:53 +0900

    jpa logging 추가, jpa cache 처리 추가

--- src/main/java/com/htdp1/deptspring/dept/repository/DepartmentRepository.java
+++ src/main/java/com/htdp1/deptspring/dept/repository/DepartmentRepository.java
@@ -1,6 +1,11 @@
 package com.htdp1.deptspring.dept.repository;
 
+import java.util.Optional;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -11,4 +16,18 @@ public interface DepartmentRepository extends CrudRepository<Department, String>
 	@Override
 	@Cacheable(cacheNames = "departments", key = "'findAll'", cacheManager = "cacheManager")
 	Iterable<Department> findAll();
+
+	@Cacheable(cacheNames = "departments", key = "#deptNo", cacheManager = "cacheManager")
+	Optional<Department> findById(String deptNo);
+
+	@CachePut(cacheNames = "departments", key = "#department.deptNo", cacheManager = "cacheManager")
+	@CacheEvict(cacheNames = "departments", key = "'findAll'", cacheManager = "cacheManager")
+	<S extends Department> S save(Department department);
+
+	@Caching(evict = { 
+			@CacheEvict(cacheNames = "departments", key = "'findAll'", cacheManager = "cacheManager"),
+			@CacheEvict(cacheNames = "departments", key = "#deptNo", cacheManager = "cacheManager") 
+			}
+	)
+	void deleteById(String deptNo);
 }
