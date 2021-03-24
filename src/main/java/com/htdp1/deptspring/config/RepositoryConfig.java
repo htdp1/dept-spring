f628ab41509b7655bbd856e516699479fa7993b3
h-soon.kim <noisonnoiton@gmail.com>
2021-03-24 18:17:08 +0900

    edit port

--- src/main/java/com/htdp1/deptspring/config/RepositoryConfig.java
+++ src/main/java/com/htdp1/deptspring/config/RepositoryConfig.java
@@ -1,22 +1,16 @@
 package com.htdp1.deptspring.config;
 
-import java.time.Duration;
-
-import com.htdp1.deptspring.dept.model.DepartmentRedis;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
+import com.htdp1.deptspring.dept.model.DepartmentRedis;
 
 @Configuration
 @EnableRedisRepositories
@@ -31,19 +25,7 @@ public class RepositoryConfig {
 		redisStandaloneConfiguration.setHostName(host);
 		redisStandaloneConfiguration.setPort(port);
 
-		LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
-		.clientOptions(
-			ClientOptions.builder()
-				.socketOptions(
-					SocketOptions.builder()
-						.connectTimeout(Duration.ofMillis(10000))
-						.build())
-				.build())
-		.commandTimeout(Duration.ofSeconds(10000))
-		.build();
-
-		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfiguration);
-		// JedisConnectionFactory connectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
 
 		return connectionFactory;
 	}
